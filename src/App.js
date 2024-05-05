757fbc3cb34898b135c9445f5a5c6c856e98d81c
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-05-05 12:19:32 -0300

    Feat: darkMode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
+
 const App = () => {
   return (
-    <div className="App">
-
+    <div className="h-screen dark:bg-neutral-700">
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
